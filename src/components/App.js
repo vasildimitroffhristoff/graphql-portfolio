2380a9e693af073e12e700c0dfc773b5ba3c4000
Vasil Hristov <vasildimitrovhristov@gmail.com>
2019-12-24 11:44:18 +0200

    Lottie animation and routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 
 import { GlobalStyles } from '../theme'
+import About from './About'
 import Header from './Header'
+import Landing from './Landing'
 
 function App() {
   return (
-    <>
+    <Router>
       <GlobalStyles />
       <Header />
-    </>
+      <Switch>
+        <Route exact path="/">
+          <Landing />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
 
