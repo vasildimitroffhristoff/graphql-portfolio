a71e58f37be11d47f23e1f6a9cfecebafbb9449c
Vasil Hristov <vasildimitrovhristov@gmail.com>
2020-01-12 10:41:53 +0200

    Remove code animation on about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,22 +6,16 @@ import gitCommits from '../../assets/git-commits.png'
 import me from '../../assets/me.svg'
 import Resume from '../../assets/vh_cv.pdf'
 import { Wrapper } from '../../theme'
-import { AboutSection, Code, SocialLinks } from './AboutStyles'
+import { AboutSection, SocialLinks } from './AboutStyles'
 import { ABOUT } from './graphql'
 
 function AboutComponent() {
-  const [greetingText, setGreetingText] = useState('')
   const { loading, error, data } = useQuery(ABOUT, {
     variables: { id: 'ck4h6pxfvasyx0b66zim26s2i' }
   })
   if (loading || error || !data) return <div>...</div>
 
   const { whoAmI, whatIDo } = data.about
-
-  const handleGreetingText = haveWeMet => {
-    const text = haveWeMet ? `Nice to see you!` : `Nice to meet you!`
-    setGreetingText(text)
-  }
 
   return (
     <div className="page">
@@ -31,55 +25,19 @@ function AboutComponent() {
             <img className="me" src={me} alt="me" />
             <h2>
               <i className="icon fas fa-terminal"></i>
-              {greetingText.length === 0 && (
-                <Typist
-                  cursor={{
-                    show: true,
-                    blink: true,
-                    hideWhenDone: true
-                  }}
-                >
-                  <Typist.Delay ms={1000} />
-                  <span>who am I</span>
-                  <Typist.Backspace count={8} delay={400} />
-                  <span>whoami</span>
-                </Typist>
-              )}
-              {greetingText.length > 0 && (
-                <Typist>
-                  <span style={{ fontSize: '2.8rem' }}>{greetingText}</span>
-                </Typist>
-              )}
+              <Typist
+                cursor={{
+                  show: true,
+                  blink: true,
+                  hideWhenDone: true
+                }}
+              >
+                <Typist.Delay ms={1000} />
+                <span>who am I</span>
+                <Typist.Backspace count={8} delay={400} />
+                <span>whoami</span>
+              </Typist>
             </h2>
-
-            <Code className="greeting-code">
-              <span>
-                <i></i>
-                <i></i>
-                <i></i>
-              </span>
-              <p>
-                <b>
-                  {`have_We_Met.before`}
-                  <button
-                    className="btn-boolean true"
-                    onClick={() => handleGreetingText(true)}
-                  >
-                    true
-                  </button>
-                  <button
-                    className="btn-boolean false"
-                    onClick={() => handleGreetingText(false)}
-                  >
-                    false
-                  </button>
-                </b>
-                <br />
-                &nbsp;? <i>'Nice to see you!'</i>
-                <br />
-                &nbsp;: <i>'Nice to meet you!';</i>
-              </p>
-            </Code>
           </header>
 
           <p
